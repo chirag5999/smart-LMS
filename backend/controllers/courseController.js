cebf1e6dc28e77f3ea37d54807ef1a0f0a6671f7
Chirag Bansal <chirag.bansal5999@gmail.com>
2025-12-16 21:46:04 +0530

    complete authContext and CourseDetail page

--- backend/controllers/courseController.js
+++ backend/controllers/courseController.js
@@ -1,23 +1,42 @@
 import Course from "../models/CourseModel.js";
 
+export const getCourse = async (req, res) => {
+    const {keyword, category} = req.query;
+    console.log("keyword, category", keyword, category)
 
-export const getCourse = async(req,res) => {
-    try{
-        const courses = await Course.find({}).populate('category','name').populate('instructor','name');
+    // title filter
+    const titleFilter = keyword ? { title: { $regex: keyword, $options: 'i'} } : {};
+    console.log("titleFilter", titleFilter)
+    
+    // category filter: TODO:: check the filter
+    // const categoryFilter = category ? {category}: {};
+    // console.log("categoryFilter", categoryFilter)
+
+    try {
+        // fetch the courses from the database
+        const courses = await Course.find({...titleFilter})
+            .populate('category', 'name')
+            .populate('instructor', 'name');
+            
         return res.status(200).json(courses);
-    }catch(err){
-        return res.status(500).json({message:"Internal Server Error"})
+    } catch (error) {
+        return res.status(500).json({message: 'Server error!'})
     }
 }
 
-export const getCourseById = async(req,res) => {
-    try{
-        const course = await Course.findById(req.params.id).populate('category','name').populate('instructor','name');
-        if(!course){
-            return res.status(404).json({message:"Course not found!!!"});
+export const getCourseById = async (req, res) => {
+    try {
+        // fetch the courses from the database
+        const course = await Course.findById(req.params.id)
+            .populate('category', 'name')
+            .populate('instructor', 'name');
+        
+        if (!course) {
+            return res.status(404).json({message: "Course not found!"});
         }
+            
         return res.status(200).json(course);
-    }catch(err){
-        return res.status(500).json({message:"Internal Server Error"});
+    } catch (error) {
+        return res.status(500).json({message: 'Server error!'})
     }
 }
